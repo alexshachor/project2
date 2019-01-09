ca0fdde66ebb413f9a69f54c84b4044d997ff561
aviMiletzky <avimiletzky@gmail.com>
2019-01-09 20:34:41 +0200

    add Blabla

---
+++ Blabla.cpp
@@ -0,0 +1,5 @@
+//
+// Created by avi on 1/9/19.
+//
+
+#include "Blabla.h"
---
+++ Blabla.h
@@ -0,0 +1,14 @@
+//
+// Created by avi on 1/9/19.
+//
+
+#ifndef PROJECT2_BLABLA_H
+#define PROJECT2_BLABLA_H
+
+
+class Blabla {
+
+};
+
+
+#endif //PROJECT2_BLABLA_H
